db1cd98b4d03af940cc66cdcb541d05029d2b962
yosefHesham <yosef.seven1998@gmail.com>
2022-01-02 11:28:08 +0200

    odify_uri

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 let mongoose;
 try {
   mongoose = require("mongoose");
-  mongoose.connect(process.env.MONGO_URL, {
+  mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
