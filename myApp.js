6c27935eb9e7ee3248aaf5483d6160932d85eb40
yosefHesham <yosef.seven1998@gmail.com>
2022-01-03 23:18:23 +0200

    find and remove

--- myApp.js
+++ myApp.js
@@ -75,7 +75,7 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, done);
 };
 
 const removeManyPeople = (done) => {
