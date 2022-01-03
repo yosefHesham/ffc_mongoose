0df656c8225d6c10534afb4851ddfd28f7c26bb8
yosefHesham <yosef.seven1998@gmail.com>
2022-01-03 23:16:57 +0200

    find and update

--- myApp.js
+++ myApp.js
@@ -66,8 +66,12 @@ const findEditThenSave = (personId, done) => {
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate(
+    { name: personName },
+    { age: ageToSet },
+    { new: true },
+    done
+  );
 };
 
 const removeById = (personId, done) => {
